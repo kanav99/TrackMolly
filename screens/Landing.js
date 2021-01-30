f31166ae283a4866a7007501d04fdd3f6c03c193
Kanav Gupta <kanav0610@gmail.com>
2021-01-31 02:19:28 +0530

    landing page

--- screens/Landing.js
+++ screens/Landing.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import {SafeAreaView, View, StyleSheet, Text, TextInput} from 'react-native';
-import Balls from './Balls';
-import styled from 'styled-components';
-import LongButton from './LongButton';
-import globalData from '../Globals';
-import auth from '@react-native-firebase/auth';
+
+import MapView from 'react-native-maps';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import {TabBar} from './TabBar';
+
+const Tab = createBottomTabNavigator();
+
+const MapTab = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <MapView
+        style={{flex: 1}}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      <View style={styles.topBar}></View>
+    </SafeAreaView>
+  );
+};
 
 class Landing extends React.Component {
   constructor(props) {
@@ -16,8 +35,28 @@ class Landing extends React.Component {
   }
 
   render() {
-    return <SafeAreaView></SafeAreaView>;
+    return (
+      <Tab.Navigator tabBar={(props) => <TabBar {...props} />}>
+        <Tab.Screen name="Map" component={MapTab} />
+        <Tab.Screen name="Logs" component={MapTab} />
+        <Tab.Screen name="Saviours" component={MapTab} />
+        <Tab.Screen name="Settings" component={MapTab} />
+      </Tab.Navigator>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  topBar: {
+    position: 'absolute',
+    width: '100%',
+    height: 74,
+    top: 0,
+    left: 0,
+    backgroundColor: '#6739B7',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+});
 
 export default Landing;
---
+++ screens/TabBar.js
@@ -0,0 +1,134 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  TouchableOpacity,
+  Dimensions,
+  StyleSheet,
+  Animated,
+  Text,
+} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const iconMap = (label) => {
+  switch (label) {
+    case 'Map':
+      return 'place';
+    case 'Logs':
+      return 'history';
+    case 'Saviours':
+      return 'people';
+    case 'Settings':
+      return 'settings';
+    default:
+      return '';
+  }
+};
+
+export function TabBar({state, descriptors, navigation}) {
+  const focusedOptions = descriptors[state.routes[state.index].key].options;
+
+  if (focusedOptions.tabBarVisible === false) {
+    return null;
+  }
+
+  const totalWidth = Dimensions.get('window').width;
+  const tabWidth = totalWidth / state.routes.length;
+
+  const [translateValue] = useState(new Animated.Value(0));
+
+  const animateSlider = (index) => {
+    Animated.spring(translateValue, {
+      toValue: index * tabWidth,
+      velocity: 10,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  useEffect(() => {
+    animateSlider(state.index);
+  }, [state.index]);
+
+  return (
+    <View style={{flexDirection: 'row'}}>
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+
+          animateSlider(index);
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? {selected: true} : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={style.bottomItemStyle}>
+            <MaterialIcons
+              name={iconMap(label)}
+              size={32}
+              style={{color: 'white'}}
+            />
+            <Text style={{color: '#ffffff'}}>{label}</Text>
+          </TouchableOpacity>
+        );
+      })}
+      <Animated.View
+        style={[
+          style.slider,
+          {
+            transform: [{translateX: translateValue}],
+            width: tabWidth - 20,
+          },
+        ]}
+      />
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  bottomItemStyle: {
+    flex: 1,
+    height: 70,
+    alignContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#6739B7',
+    paddingTop: 10,
+  },
+  slider: {
+    height: 2,
+    position: 'absolute',
+    bottom: 0,
+    left: 10,
+    backgroundColor: '#03DAC5',
+    borderRadius: 10,
+    width: 90,
+  },
+});
